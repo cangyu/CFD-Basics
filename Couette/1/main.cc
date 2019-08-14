e877cdab6d87fcd67d7a5a9e3ab5745f21486494
Yu Cang <yhcy1993@gmail.com>
2019-08-14 20:36:13 +0800

    seems working: but sensitive to parameters like rho, mu

--- Couette/1/main.cc
+++ Couette/1/main.cc
@@ -53,17 +53,27 @@ public:
 	{
 		std::fill(data.begin(), data.end(), x);
 	}
+
+	double max() const
+	{
+		return *max_element(data.begin(), data.end());
+	}
+
+	double min() const
+	{
+		return *min_element(data.begin(), data.end());
+	}
 };
 
-const size_t WIDTH = 18;
+const size_t WIDTH = 12;
 const size_t DIGITS = 7;
 
-const double L = 0.5*0.3048; // m
-const double D = 0.01*0.3048; // m
-const double Ue = 1.0 * 0.3048; // m/s
-const double rho = 1.225055; // Kg/m3
+const double L = 0.5; // m
+const double D = 0.01; // m
+const double Ue = 1.0; // m/s
+const double rho = 0.002377; // Kg/m3
 const double Re = 63.6;
-const double mu = rho * Ue * L / Re; // Kg/m/s
+const double mu = 3.737e-7; // Kg/m/s
 
 const int Nx = 21, Ny = 11;
 const double dx = L / (Nx - 1), dy = D / (Ny - 1);
@@ -74,7 +84,7 @@ vector<double> x(Nx, 0.0), y(Ny, 0.0);
 const double dt = 0.001;
 double t = 0.0;
 int iter_cnt = 0;
-const int MAX_ITER_NUM = 2000;
+const int MAX_ITER_NUM = 500;
 
 const double a = 2 * (dt / dxdx + dt / dydy);
 const double b = -dt / dxdx;
@@ -105,6 +115,8 @@ void write_result(void)
 
 void init(void)
 {
+	cout << "mu=" << mu << endl;
+
 	// Coordinates
 	for (int i = 1; i < Nx; ++i)
 		x[i] = L * i / (Nx - 1);
@@ -112,10 +124,10 @@ void init(void)
 		y[j] = D * j / (Ny - 1);
 
 	// Velocity field
-	for(int i = 1; i <= Nx; ++i)
-		u(i, Ny) = Ue;
+	for (int i = 1; i <= Nx + 1; ++i)
+		u_star(i, Ny) = u(i, Ny) = Ue;
 
-	v_star(15, 5) = 0.5*0.3048; // Initial peak
+	v(15, 5) = v_star(15, 5) = 0.5; // Initial peak to ensure 2D flow structure
 
 	// Header
 	result.open("flow.txt");
@@ -134,9 +146,38 @@ void finalize(void)
 	result.close();
 }
 
-void loop(void)
+void JacobiMethod()
 {
-	// rhou_star at inner points
+	for (int r = 0; r < 200; ++r)
+	{
+		auto pp = p_prime;
+		for (int i = 2; i < Nx; ++i)
+			for (int j = 2; j < Ny; ++j)
+			{
+				double d = (rho*u_star(i + 1, j) - rho * u_star(i, j)) / dx + (rho*v_star(i + 1, j + 1) - rho * v_star(i + 1, j)) / dy;
+				p_prime(i, j) = -(b * pp(i + 1, j) + b * pp(i - 1, j) + c * pp(i, j + 1) + c * pp(i, j - 1) + d) / a;
+			}
+
+		//cout << "\tInternal loop:" << r + 1 << ": Max(p\')=" << p_prime.max() << " Min(p\')=" << p_prime.min() << endl;
+	}
+}
+
+void GaussSeidelMethod()
+{
+	for (int r = 0; r < 200; ++r)
+	{
+		for (int i = 2; i < Nx; ++i)
+			for (int j = 2; j < Ny; ++j)
+			{
+				double d = (rho*u_star(i + 1, j) - rho * u_star(i, j)) / dx + (rho*v_star(i + 1, j + 1) - rho * v_star(i + 1, j)) / dy;
+				p_prime(i, j) = -(b * p_prime(i + 1, j) + b * p_prime(i - 1, j) + c * p_prime(i, j + 1) + c * p_prime(i, j - 1) + d) / a;
+			}
+	}
+}
+
+void SIMPLE(void)
+{
+	// u_star at inner points
 	for(int j = 2; j <= Ny-1; ++j)
 		for (int i = 2; i <= Nx; ++i)
 		{
@@ -149,12 +190,19 @@ void loop(void)
 			double t22 = u(i, j + 1) - 2 * u(i, j) + u(i, j - 1);
 			double A_star = -(t11 / dx2 + t12 / dy2) + mu * (t21 / dxdx + t22 / dydy);
 			
-			double rhou_star = rho * u_star(i, j) + A_star * dt - dt / dx * (p_star(i, j) - p_star(i - 1, j));
-			u_star(i, j) = rhou_star / rho;
+			double loc_rhou_star = rho * u_star(i, j) + A_star * dt - dt / dx * (p_star(i, j) - p_star(i - 1, j));
+			u_star(i, j) = loc_rhou_star / rho;
 		}
 
-	// rhov_star at inner points
-	for(int i = 3; i <= Nx; ++i)
+	// u_star at virtual points, transparent
+	for (int j = 1; j <= Ny; ++j)
+	{
+		u_star(1, j) = u_star(2, j); // Inlet
+		u_star(Nx + 1, j) = u_star(Nx, j); // Outlet
+	}
+
+	// v_star at inner points
+	for (int i = 2; i <= Nx + 1; ++i)
 		for (int j = 2; j <= Ny; ++j)
 		{
 			double u_bar1 = 0.5 *(u(i, j - 1) + u(i, j));
@@ -166,30 +214,47 @@ void loop(void)
 			double t22 = v(i, j + 1) - 2 * v(i, j) + v(i, j - 1);
 			double B_star = -(t11 / dx2 + t12 / dy2) + mu * (t21 / dxdx + t22 / dydy);
 
-			double rhov_star = rho * v_star(i, j) + B_star * dt - dt / dy * (p_star(i - 1, j) - p_star(i - 1, j - 1));
-			v_star(i, j) = rhov_star / rho;
+			double loc_rhov_star = rho * v_star(i, j) + B_star * dt - dt / dy * (p_star(i - 1, j) - p_star(i - 1, j - 1));
+			v_star(i, j) = loc_rhov_star / rho;
 		}	
 
-	// Solve p_prime at inner points using relaxation method
-	for (int r = 0; r < 200; ++r)
+	// v_star at virtual points
+	for (int j = 1; j <= Ny + 1; ++j)
 	{
-		auto pp = p_prime;
-		for (int i = 2; i < Nx; ++i)
-			for (int j = 2; j < Ny; ++j)
-			{
-				double d = (rho*u_star(i + 1, j) - rho * u_star(i, j)) / dx + (rho*v_star(i + 1, j + 1) - rho * v_star(i + 1, j)) / dy;
-				p_prime(i, j) = -(b * pp(i + 1, j) + b * pp(i - 1, j) + c * pp(i, j + 1) + c * pp(i, j - 1) + d) / a;
-			}
+		v_star(Nx + 2, j) = v_star(Nx + 1, j); // Outlet
 	}
 
-	// Correct p, u, v
+	// Solve p_prime at inner points
+	JacobiMethod();
+
+	// Correct p
 	for (int i = 2; i < Nx; ++i)
 		for (int j = 2; j < Ny; ++j)
+		{
+			// Update with relaxation
 			p(i, j) = p_star(i, j) + alpha_p * p_prime(i, j);
+			
+			// For next round
+			p_star(i, j) = p(i, j);
+		}
+
+	// Correct u
+	for (int j = 2; j <= Ny - 1; ++j)
+		for (int i = 1; i <= Nx + 1; ++i)
+			u(i, j) = u_star(i, j);
+
+	// Correct v
+	for (int i = 2; i <= Nx + 2; ++i)
+		for (int j = 2; j <= Ny; ++j)
+			v(i, j) = v_star(i, j);
 }
 
 bool check_convergence(void)
 {
+	cout << "Max(u)=" << u.max() << " Min(u)=" << u.min() << endl;
+	cout << "Max(v)=" << v.max() << " Min(v)=" << v.min() << endl;
+	cout << "Max(p)=" << p.max() << " Min(p)=" << p.min() << endl;
+
 	return iter_cnt > MAX_ITER_NUM;
 }
 
@@ -200,7 +265,7 @@ void solve(void)
 	{
 		++iter_cnt;
 		cout << "Iter" << iter_cnt << ":" << endl;
-		loop();
+		SIMPLE();
 		t += dt;
 		write_result();
 		converged = check_convergence();
